7ea5714ead076492a83017e0fd4b1c76f29bc9e4
Nilton da Silva Barros <barros.nilton@yahoo.com.br>
2024-06-18 14:20:23 -0300

    Incluída tela de Splash

---
+++ app/src/main/java/com/example/app/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.example.app
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashActivity : AppCompatActivity() {
+
+    private val splashTime: Long = 2000
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(splashTime)
+
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
